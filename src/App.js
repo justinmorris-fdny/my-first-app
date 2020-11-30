e1ee34a0849d70dcfb55701d60a5bb94b6f08f82
yourUsernameForGithub <youremailhere>
2020-11-30 18:38:27 +0000

    done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+      <h1>JUSTIN MORRIS</h1>
+      <h2>Student @ FDNY HS</h2>
+    <body>
+            <br></br><h1>About Me</h1>
+    <pr>I am a high school Student</pr>
+    <ul>
+        <li>I like to play video game </li>
+        <li>I Love watch sports</li>
+        <li>I am a soccer player</li>
+        <li>fifa is my favorite game to play</li>
+    </ul>
+    <p>I live in brooklyn,New york with my Mother,sister, grandmother and uncle<br></br>I like brooklyn it is a very intersting environmet.I have alot of things i have as a hobbie that i do in my spare time.Some of these hobbies are:</p>
+   <body><br></br></body>
+    <ul>
+        <li>list to music</li>
+        <li>listen to music and read or study.</li>
+        <li>draw</li>
+        <li>listen music while drawing</li>
+        <li>watching television</li>
+        <li>watch videos on social media</li>
+
+    </ul>
+    </body>
+    <p1> I have read alot of book in my free time.one of the books that i have come to like is "The Art Of Money Getting" by P.T Barnum. <br>
+    </br>This book in summary is about financial resposibility. 
+    However is goes in details on the irresponsible action one takes or does with money and how you minimize these action,
+     that seem irresistilbe and other responsible actions.
+    i having watch many movies to have a favorite movies,
+    I have watch alot of series because I like series.
+    One of my favortie series of all time is The Office </p1></div>
+    );
 }
 
 export default App;
