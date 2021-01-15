ec0a0b113fda521ad92168ed40e6fe4f56f2aa26
yourUsernameForGithub <youremailhere>
2021-01-15 00:37:34 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
-
+import image from './Components/Images/Copy of File_000.jpeg';
 function App() {
   return (
     <div className="App">
-      <h1>JUSTIN MORRIS</h1>
-      <h2>Student @ FDNY HS</h2>
+      <h3 className="topic">About Me Page: </h3>
+      <h1 className="title">JUSTIN MORRIS</h1>
+      <img src= {image} className="photo"></img>
+      <h2 className="target">Student @ FDNY HS</h2>
     <body>
-            <br></br><h1>About Me</h1>
-    <pr>I am a high school Student</pr>
-    <ul>
-        <li>I like to play video game </li>
-        <li>I Love watch sports</li>
-        <li>I am a soccer player</li>
-        <li>fifa is my favorite game to play</li>
+        
+    <pr>I am a high school Student that:</pr>
+    <ul className="list">
+        <li>like to play video game </li>
+        <li>Love watch sports</li>
+        <li>Plays soccer</li>
+        <li>Thinks fifa is my favorite game to play</li>
     </ul>
     <p>I live in brooklyn,New york with my Mother,sister, grandmother and uncle<br></br>I like brooklyn it is a very intersting environmet.I have alot of things i have as a hobbie that i do in my spare time.Some of these hobbies are:</p>
-   <body><br></br></body>
-    <ul>
+   <body></body>
+    <ul className="habits">
         <li>list to music</li>
         <li>listen to music and read or study.</li>
         <li>draw</li>
@@ -27,7 +29,8 @@ function App() {
 
     </ul>
     </body>
-    <p1> I have read alot of book in my free time.one of the books that i have come to like is "The Art Of Money Getting" by P.T Barnum. <br>
+    
+    <p1 className="body"> I have read alot of book in my free time.one of the books that i have come to like is "The Art Of Money Getting" by P.T Barnum. <br>
     </br>This book in summary is about financial resposibility. 
     However is goes in details on the irresponsible action one takes or does with money and how you minimize these action,
      that seem irresistilbe and other responsible actions.
